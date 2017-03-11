ec1a7fa01246bc377e62f11e8a27b18ce2e608ff
Meghana_Valluri <mvalluri@andrew.cmu.edu>
2017-03-11 18:55:51 -0500

    basic relationships, validations, and methods for action_idea_comment and action_idea_vote

--- handprinter/app/models/action_idea_comment.rb
+++ handprinter/app/models/action_idea_comment.rb
@@ -1,2 +1,20 @@
 class ActionIdeaComment < ActiveRecord::Base
+
+  # Relationships
+  # -----------------------------
+  belongs_to :action_idea
+  belongs_to :user
+
+  # Validations
+  # -----------------------------
+  # make sure required fields are present
+  validates_presence_of :user, :action_idea
+  
+
+  # Misc Methods
+  #-------------------------------
+  def numComments(actionIdeaID) 
+  	return ActionIdeaComment.where(actionIdeaID =self.action_idea_id).count()
+  end
+
 end
--- handprinter/app/models/action_idea_vote.rb
+++ handprinter/app/models/action_idea_vote.rb
@@ -1,2 +1,20 @@
 class ActionIdeaVote < ActiveRecord::Base
+
+  # Relationships
+  # -----------------------------
+  belongs_to :action_idea
+  belongs_to :user
+
+  # Validations
+  # -----------------------------
+  # make sure required fields are present
+  validates_presence_of :user, :action_idea
+  
+
+  # Misc Methods
+  #-------------------------------
+  def numVotes(actionIdeaID)
+  	return ActionIdeaVote.where(actionIdeaID = self.action_idea_id).count()
+  end
+
 end
